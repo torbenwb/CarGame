1da4141548f223ea824cca2cba61014d34feabc2
twbernhard <57496364+twbernhard@users.noreply.github.com>
2022-03-24 07:48:26 -0700

    small changes

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
     public int score = 0;
     public int highScore = 5;
     public float timer = 10f;
+
+    public Text scoreDisplay;
+    public Text timerDisplay;
 
     // AddScore increments the value of score by 1.
     public void AddScore()
@@ -16,6 +20,7 @@ public class Score : MonoBehaviour
             
             score++;
             print("Score: " + score);
+            scoreDisplay.text = "Score: " + score;
             
             if (score > highScore)
             {
@@ -33,5 +38,6 @@ public class Score : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
+        timerDisplay.text = timer > 0.0f ? timer.ToString("0.0") : "Out of time!";
     }
 }
